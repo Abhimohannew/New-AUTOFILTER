75d35daeb6b6d78ffb402602d25ad145bbc4770a
Abhimohannew <90506556+Abhimohannew@users.noreply.github.com>
2024-10-27 10:32:47 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -104,7 +104,7 @@ def get_reply_markup(query):
     buttons = [
         [
             InlineKeyboardButton('🔎 𝖲𝖾𝖺𝗋𝖼𝗁 𝖠𝗀𝖺𝗂𝗇', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('🎬 𝖴𝗉𝖽𝖺𝗍𝖾𝗌', url='https://t.me/FF_OTT_Updates4')
+            InlineKeyboardButton('🎬 𝖴𝗉𝖽𝖺𝗍𝖾𝗌', url='https://t.me/FF_OTT_Updates5')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
